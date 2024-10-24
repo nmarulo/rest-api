fc61a61710e924cb77cc8ffddad5ef2b42284b9f
Nicolás Marulanda P. <nmarulo@outlook.com>
2024-10-24 21:59:16 +0200

    up

---
+++ src/main/java/red/softn/npedidos/utils/test.java
@@ -0,0 +1,38 @@
+package red.softn.npedidos.utils;
+
+public abstract class Test {
+
+    public static void test() {
+        int a = 0;
+    }
+
+    public void test0() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            
+            return;
+        }
+    }
+    
+    public void test1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            
+            return;
+        }
+    }
+    
+}
